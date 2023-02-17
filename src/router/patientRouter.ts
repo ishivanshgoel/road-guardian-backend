e0559986d5b16ac4a590d1c123b8c9436c3847c4
ishivanshgoel <shivanshgoel21@gmail.com>
2023-02-18 00:15:51 +0530

    complete patient router

---
+++ src/router/patientRouter.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { PatientController } from "../controller";
+const router = express.Router();
+const patientController = new PatientController();
+
+router.route("/register").post(patientController.registerNewPatient);
+router.route("/login").post(patientController.loginPatient);
+router.route("report/share-with-doctor").post(patientController.shareReportWithDoctor);
+router.route("/report/new").post(patientController.createNewReport);
+
+router.route("/report/all").get(patientController.getAllReports);
+router.route("/report/doctor-remarks").get(patientController.getDoctorRemarksOnReports);
+
+export { router as AuthController }
